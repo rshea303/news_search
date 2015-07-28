3a2993da46f6d3231062d6a38899ce5446d03664
Rich Shea <richcshea@gmail.com>
2015-07-28 14:49:42 -0600

    add alchemyservice class

--- app/services/alchemy_service.rb
+++ app/services/alchemy_service.rb
@@ -1,8 +1,10 @@
+require 'hurley'
+
 class AlchemyService
   attr_reader :connection
 
   def initialize
-    @connection = Hurley::Client.new("https://access.alchemyapi.com/calls/data/GetNews?apikey=#{ENV[API_KEY]}&outputMode=json")
+    @connection = Hurley::Client.new("https://access.alchemyapi.com/calls/data/GetNews?apikey=#{ENV["API_KEY"]}&outputMode=json&start=now-1d&end=now&maxResults=15&return=enriched.url.title,enriched.url.text")
   end
 
   def articles
